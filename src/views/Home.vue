3861c406958cb4760aa90ecf0df5b021c38854c2
Jesse Maingi <maingijesse@gmail.com>
2020-05-04 13:12:39 +0300

    added tailwindcss

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,23 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home flex justify-center flex-col items-center">
+    <h1 class="text-4xl text-gray-900">Welcome</h1>
+    <div class="flex flex-col items-center bg-gray-400 p-10 rounded-lg shadow-lg">
+      <img src="https://via.placeholder.com/500x500" class="h-64 w-64 rounded-full" />
+      <h5 class="text-sm font-semibold">Some Image Information</h5>
+      <p class="text-center mt-4">
+        <span class="text-purple-700 font-extrabold">Lorem ipsum</span> dolor sit amet consectetur adipisicing elit. Omnis repellendus at neque laborum nesciunt aperiam iusto natus repudiandae tempora reiciendis.
+      </p>
+      <button class="bg-red-900 p-4 mt-4 text-white shadow">Learn More</button>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// @ is an alias to /src 1935
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "home",
+  components: {}
+};
 </script>
+
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: {
+    enabled: process.env.NODE_ENV === "production",
+    content: ["./public/**/*.html", "./src/**/*.vue"],
+    whitelistPatterns: [
+      /-(leave|enter|appear)(|-(to|from|active))$/,
+      /^(?!(|.*?:)cursor-move).+-move$/,
+      /^router-link(|-exact)-active$/
+    ]
+  },
+  theme: {
+    extend: {}
+  },
+  variants: {},
+  plugins: []
+};
